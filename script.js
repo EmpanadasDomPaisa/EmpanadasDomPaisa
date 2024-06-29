37155bb38a81afd27a2ccb3d0ba68836035a711f
EmpanadasDomPaisa <rg992296@gmail.com>
2024-06-29 17:57:10 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+// No se requiere JavaScript adicional para mostrar las imágenes una por una
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Puedes agregar funcionalidades interactivas aquí si es necesario
+});
